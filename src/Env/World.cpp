eb209e361a10cd7390328f9eb07c17e2d461ab0a
notVictorr <victor.galhaud@gmail.com>
2022-03-25 17:19:07 +0100

    P2.1 : World
    Generation of rock environment

--- src/Env/World.cpp
+++ src/Env/World.cpp
@@ -10,24 +10,24 @@ float World::getSize() const{
 
 void World::reloadConfig(){
     nb_cells = getAppConfig().world_cells;
-    cell_size = (getAppConfig().world_size / nb_cells);
+    cell_size = ((getAppConfig().world_size) / nb_cells);
     std::vector<Kind> new_grid((getAppConfig().world_size*cell_size), Kind::roche);
     cells_ = new_grid;
-}
-
-void World::drawOn(sf::RenderTarget& target){
-    sf::Sprite cache(renderingCache_.getTexture());
-    target.draw(cache);
 }
 
 void World::reloadCacheStructure(){
         auto test(getValueConfig()["simulation"]["world"]["textures"]);            // A AJOUTER : raccourci dans config.cpp
         int size(nb_cells*cell_size);
-        std::vector<sf::Vertex> vertexes(generateVertexes(test, nb_cells, cell_size));
+        std::vector<sf::Vertex> vertexes(generateVertexes(getValueConfig()["simulation"]["world"]["textures"], nb_cells, cell_size));
         grassVertexes_ = vertexes;
         waterVertexes_ = vertexes;
         rockVertexes_ = vertexes;
         renderingCache_.create(size, size);
+}
+
+void World::drawOn(sf::RenderTarget& target){
+    sf::Sprite cache(renderingCache_.getTexture());
+    target.draw(cache);
 }
 
 void World::updateCache(){
@@ -41,6 +41,7 @@ void World::updateCache(){
 void World::reset(bool regenerate){
     reloadConfig();
     reloadCacheStructure();
+    updateCache();
 }
 
 
