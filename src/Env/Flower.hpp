2e77131f2cd7f50e23b48ea218b8049d7eb68400
notVictorr <victor.galhaud@gmail.com>
2022-04-05 20:35:49 +0200

    p3.2 Class Flower
    tres léger début

---
+++ src/Env/Flower.hpp
@@ -0,0 +1,16 @@
+#ifndef FLOWER_HPP
+#define FLOWER_HPP
+#include <SFML/Graphics.hpp>
+
+class Flower  : [public]  Collider{};
+{
+public:
+
+
+private :
+
+    double pollen_quantity;
+
+};
+
+#endif // FLOWER_HPP
